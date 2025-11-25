2c210cbfaac7725c3883b70292386fdef6ee06ad
PrinceTyo <kresnaale2@gmail.com>
2025-11-25 13:16:59 +0700

    feat: make drawer responsive on mobile and add size 'sm' and 'md' to trending-product card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import AdvantageSection from "@/components/sections/advantage-section";
 import BundleSection from "@/components/sections/bundle-section";
+import FakeSection from "@/components/sections/fake-section";
 import StyleSection from "@/components/sections/style-section";
 import TrendingSection from "@/components/sections/trending-section";
 
 export default function Home() {
   return (
     <>
-      <TrendingSection />
+      {/* <TrendingSection /> */}
+      <FakeSection />
       <AdvantageSection />
       <StyleSection />
       <BundleSection />
--- src/components/card/trending-product.tsx
+++ src/components/card/trending-product.tsx
@@ -25,11 +25,13 @@ interface TrendingProductProps {
     sizes: string[];
     colors: { label: string; bgImg: string; bgColor: string }[];
   };
+  size?: "sm" | "md";
 }
 
 export default function TrendingProduct({
   product,
   className,
+  size,
 }: TrendingProductProps) {
   const [selectedImage, setSelectedImage] = useState<string>("");
   const [activeColorIndex, setActiveColorIndex] = useState<number | null>(null);
@@ -78,23 +80,46 @@ export default function TrendingProduct({
     setSelectedImage(product.colors[newIndex].bgImg);
   };
 
+  const sizeClass = {
+    sm: {
+      wrapper: "lg:max-w-37",
+      dekstop: "lg:w-37 lg:h-45",
+      mobile: "w-60 h-80",
+      quickView: "lg:w-32",
+      sizeP: "px-2",
+    },
+    md: {
+      wrapper: "lg:max-w-57",
+      dekstop: "lg:w-57 lg:h-72",
+      mobile: "w-75 h-96",
+      quickView: "lg:w-52",
+      sizeP: "px-4",
+    },
+  }[size || "md"];
+
   return (
     <>
-      <div className={`w-75 lg:max-w-37 space-y-3 shrink-0 ${className || ""}`}>
+      <div
+        className={`w-75 ${sizeClass.wrapper} space-y-3 shrink-0 ${
+          className || ""
+        }`}
+      >
         <div className="bg-gray-400 w-fit border border-gray-500 rounded-xl lg:rounded-3xl relative overflow-hidden group">
           <img
             src={selectedImage || product.images.front}
-            className="w-75 h-96 lg:w-37 lg:h-45 object-cover rounded-xl lg:rounded-3xl transition-all duration-700 ease-out group-hover:opacity-0 group-hover:scale-105"
+            className={`${sizeClass.mobile} ${sizeClass.dekstop} object-cover rounded-xl lg:rounded-3xl transition-all duration-700 ease-out group-hover:opacity-0 group-hover:scale-105`}
             alt=""
           />
           <img
             src={product.images.hover}
-            className="w-75 h-96 lg:w-37 lg:h-45 object-cover rounded-xl lg:rounded-3xl absolute inset-0 opacity-0 transition-all duration-700 ease-out group-hover:opacity-100 group-hover:scale-105"
+            className={`${sizeClass.mobile} ${sizeClass.dekstop} object-cover rounded-xl lg:rounded-3xl absolute inset-0 opacity-0 transition-all duration-700 ease-out group-hover:opacity-100 group-hover:scale-105`}
             alt=""
           />
 
           <div className="absolute inset-0 flex items-end justify-center text-black text-[10px] font-medium font-inter opacity-100 translate-y-0 lg:opacity-0 lg:translate-y-full transition-all duration-400 ease-out lg:group-hover:opacity-100 lg:group-hover:translate-y-0">
-            <div className="w-71.5 lg:w-32 group/quickview transition-all duration-300 ease-out lg:group-hover/quickview:-translate-y-16">
+            <div
+              className={`w-71.5 ${sizeClass.quickView} group/quickview transition-all duration-300 ease-out lg:group-hover/quickview:-translate-y-16`}
+            >
               <div
                 className="flex items-center justify-between gap-2 py-2 px-2 rounded-t-md rounded-b-md lg:rounded-b-none lg:rounded-t-xl bg-gray-100 cursor-pointer mb-1.5 lg:mb-0"
                 onClick={handleQuickViewClick}
@@ -110,7 +135,7 @@ export default function TrendingProduct({
                     {product.sizes.map((size) => (
                       <button
                         key={size}
-                        className="px-2 py-1 text-[10px] border border-gray-300 rounded hover:bg-black hover:text-white transition-colors"
+                        className={`${sizeClass.sizeP} py-1 text-[10px] border border-gray-300 rounded hover:bg-black hover:text-white transition-colors`}
                       >
                         {size}
                       </button>
@@ -158,16 +183,25 @@ export default function TrendingProduct({
         </div>
       </div>
 
-      <Drawer open={isDrawerOpen} direction="right" onOpenChange={handleClose}>
-        <DrawerContent className="h-170 min-w-[76vw] ml-auto rounded-3xl bg-transparent p-0 mr-6 flex items-center">
-          <div className="h-170 w-full rounded-3xl bg-white p-0 flex">
-            <div className="w-1/2 h-170 bg-gray-300 rounded-l-3xl relative overflow-hidden">
+      <Drawer
+        open={isDrawerOpen}
+        direction={
+          typeof window !== "undefined" && window.innerWidth < 1024
+            ? "bottom"
+            : "right"
+        }
+        onOpenChange={handleClose}
+      >
+        <DrawerContent className="h-[95vh] lg:h-[90vh] w-full lg:min-w-[76vw] lg:ml-auto rounded-t-3xl lg:rounded-3xl bg-transparent p-0 lg:mr-6 flex items-center border-none">
+          <div className="h-full w-full rounded-t-3xl lg:rounded-3xl bg-white p-0 flex flex-col lg:flex-row overflow-y-auto lg:overflow-hidden scrollbar-hide pb-32 lg:pb-0">
+            {/* Left Section - Image Gallery */}
+            <div className="w-full lg:w-1/2 h-[50vh] lg:h-full p-3 lg:p-0 rounded-t-3xl lg:rounded-l-3xl lg:rounded-tr-none relative overflow-hidden shrink-0">
               <div className="h-full relative">
                 {product.colors.map((color, index) => (
                   <img
                     key={index}
                     src={color.bgImg}
-                    className={`h-170 w-[38vw] object-cover rounded-l-3xl absolute inset-0 transition-all duration-500 ease-in-out ${
+                    className={`h-full w-full object-cover bg-gray-300 rounded-2xl lg:rounded-l-3xl lg:rounded-tr-none absolute inset-0 transition-all duration-500 ease-in-out ${
                       activeColorIndex === index
                         ? "opacity-100 translate-x-0"
                         : activeColorIndex === null && index === 0
@@ -181,16 +215,16 @@ export default function TrendingProduct({
                 ))}
               </div>
 
-              <div className="absolute bottom-0 left-0 right-0 p-4 rounded-bl-3xl">
-                <div className="flex items-center justify-evenly gap-3">
+              <div className="absolute bottom-0 left-0 right-0 p-3 lg:p-4">
+                <div className="flex items-center justify-between lg:justify-evenly gap-2 lg:gap-3 p-4 lg:p-0">
                   <button
                     onClick={prevImage}
-                    className="bg-black text-white p-4 rounded-full transition-all duration-300 shadow-lg shrink-0"
+                    className="bg-black text-white p-3 lg:p-4 rounded-full transition-all duration-300 shadow-lg shrink-0"
                   >
-                    <IoIosArrowBack size={20} />
+                    <IoIosArrowBack className="w-4 h-4 lg:w-5 lg:h-5" />
                   </button>
 
-                  <div className="flex items-center gap-2 overflow-x-auto scrollbar-hide">
+                  <div className="hidden lg:flex items-center gap-2 overflow-x-auto scrollbar-hide">
                     {product.colors.map((color, index) => (
                       <Tooltip key={index}>
                         <TooltipTrigger asChild>
@@ -204,7 +238,7 @@ export default function TrendingProduct({
                           >
                             <img
                               src={color.bgImg}
-                              className="w-20 h-20 object-cover bg-gray-300 rounded-lg"
+                              className="w-14 h-14 lg:w-20 lg:h-20 object-cover bg-gray-300 rounded-lg"
                               alt=""
                             />
                           </div>
@@ -218,41 +252,42 @@ export default function TrendingProduct({
 
                   <button
                     onClick={nextImage}
-                    className="bg-black text-white p-4 rounded-full transition-all duration-300 shadow-lg shrink-0"
+                    className="bg-black text-white p-3 lg:p-4 rounded-full transition-all duration-300 shadow-lg shrink-0"
                   >
-                    <IoIosArrowForward size={20} />
+                    <IoIosArrowForward className="w-4 h-4 lg:w-5 lg:h-5" />
                   </button>
                 </div>
               </div>
             </div>
 
-            <div className="w-1/2 h-170 bg-white rounded-r-3xl border border-white flex flex-col">
-              <div className="flex-1 overflow-y-auto scrollbar-hide py-6 px-8">
+            {/* Right Section - Product Details */}
+            <div className="w-full lg:w-1/2 flex-1 bg-white rounded-b-3xl lg:rounded-r-3xl lg:rounded-bl-none border border-white flex flex-col relative lg:overflow-hidden">
+              <div className="flex-1 lg:max-h-full lg:overflow-y-auto scrollbar-hide py-4 px-4 lg:py-6 lg:px-8 lg:pb-6">
                 <div>
-                  <h1 className="font-semibold text-lg font-rubik cursor-pointer hover:text-gray-400">
+                  <h1 className="font-semibold text-base lg:text-lg font-rubik cursor-pointer hover:text-gray-400">
                     {product.title}
                   </h1>
-                  <p className="text-md text-gray-800 ">SDZ1056</p>
-                  <p className="text-xl leading-9 font-inter">
+                  <p className="text-sm lg:text-md text-gray-800">SDZ1056</p>
+                  <p className="text-lg lg:text-xl leading-9 font-inter">
                     ${product.price.toLocaleString()} USD
                   </p>
                 </div>
 
-                <Separator className="my-5" />
+                <Separator className="my-4 lg:my-5" />
 
                 <div className="flex items-center space-x-1.5">
                   <FaCheck className="p-1 text-white bg-green-600 rounded-full" />
                   <p className="text-xs">30 in stock</p>
                 </div>
 
-                <div className="my-5">
+                <div className="my-4 lg:my-5">
                   <p className="font-rubik font-bold text-xs mb-2">
                     COLOR:{" "}
                     {activeColorIndex !== null
                       ? product.colors[activeColorIndex].label.toUpperCase()
                       : product.colors[0].label.toUpperCase()}
                   </p>
-                  <div className="flex items-center gap-1.5 py-1 max-w-full">
+                  <div className="flex items-center gap-1.5 py-1 max-w-full flex-wrap">
                     {product.colors.map((color, index) => (
                       <Tooltip key={index}>
                         <TooltipTrigger asChild>
@@ -275,7 +310,7 @@ export default function TrendingProduct({
                   </div>
                 </div>
 
-                <div className="my-5">
+                <div className="my-4 lg:my-5">
                   <p className="font-rubik font-bold text-xs mb-2">
                     SIZE: {selectedSize || product.sizes[0]}
                   </p>
@@ -284,7 +319,7 @@ export default function TrendingProduct({
                       <button
                         key={size}
                         onClick={() => setSelectedSize(size)}
-                        className={`px-4 py-1 text-[10px] border rounded transition-colors ${
+                        className={`px-3 lg:px-4 py-1 text-[10px] border rounded transition-colors ${
                           selectedSize === size ||
                           (!selectedSize && size === product.sizes[0])
                             ? "bg-black text-white border-black"
@@ -297,9 +332,9 @@ export default function TrendingProduct({
                   </div>
                 </div>
 
-                <div className="my-5">
+                <div className="my-4 lg:my-5">
                   <p className="font-rubik font-bold text-xs mb-2">Quantity</p>
-                  <div className="w-fit flex items-center justify-start gap-8 border border-black px-4 py-2.5 rounded-sm">
+                  <div className="w-fit flex items-center justify-start gap-6 lg:gap-8 border border-black px-3 lg:px-4 py-2 lg:py-2.5 rounded-sm">
                     <FaMinus
                       size={14}
                       className={`cursor-pointer transition-colors ${
@@ -322,47 +357,52 @@ export default function TrendingProduct({
                   </div>
                 </div>
 
-                <div className="my-5 bg-cyan-50 p-2 border border-gray-300 flex items-start justify-evenly gap-16">
-                  <div className="flex items-center gap-4">
-                    <LuBox size={22} />
+                <div className="my-4 lg:my-5 bg-cyan-50 p-2 lg:p-3 border border-gray-300 flex flex-col lg:flex-row items-start justify-between gap-3 lg:gap-16 rounded">
+                  <div className="flex items-start gap-3 lg:gap-4">
+                    <LuBox
+                      size={20}
+                      className="lg:w-[22px] lg:h-[22px] shrink-0 mt-0.5"
+                    />
                     <div>
                       <p className="font-medium text-xs">
                         Pickup available at Pakis Warehouse
                       </p>
-                      <p className="text-xs">Usually ready in 24 hours</p>
+                      <p className="text-xs text-gray-600">
+                        Usually ready in 24 hours
+                      </p>
                     </div>
                   </div>
                   <div>
-                    <p className="text-xs underline">
+                    <p className="text-xs underline cursor-pointer hover:text-gray-600">
                       Check availability at other stores
                     </p>
                   </div>
                 </div>
 
-                <div className="flex h-5 items-center gap-3">
+                <div className="flex h-5 items-center gap-1">
                   <div className="flex items-center gap-2">
-                    <LiaShippingFastSolid size={20} />
+                    <LiaShippingFastSolid size={20} className="shrink-0" />
                     <p className="text-xs">
                       Free delivery on February 7th - 13th
                     </p>
                   </div>
                   <Separator
                     orientation="vertical"
-                    className="h-6 w-px bg-gray-400"
+                    className="block h-6 w-px bg-gray-400"
                   />
                   <div className="flex items-center gap-2">
-                    <BsBoxSeam />
+                    <BsBoxSeam className="shrink-0" />
                     <p className="text-xs">Free + easy returns</p>
                   </div>
                 </div>
               </div>
 
-              <div className="border-t border-gray-200 p-6 bg-white rounded-br-3xl">
-                <div className="flex items-center justify-center gap-4">
-                  <button className="px-20 py-4 border border-black bg-black text-white hover:bg-gray-200 hover:text-black transition-all duration-300 ease-in-out rounded-full">
+              <div className="fixed lg:relative bottom-0 left-0 right-0 border-t border-gray-200 p-4 lg:p-6 bg-white rounded-b-3xl lg:rounded-br-3xl lg:rounded-bl-none z-10">
+                <div className="flex flex-col lg:flex-row items-center justify-center gap-3 lg:gap-4">
+                  <button className="w-full lg:w-auto px-12 lg:px-16 py-3 lg:py-4 border border-black bg-black text-white hover:bg-gray160 hover:text-black transition-all duration-300 ease-in-out rounded-full text-sm lg:text-base font-medium text-nowrap">
                     Add to cart
                   </button>
-                  <button className="px-20 py-4 border border-black bg-black text-white hover:bg-gray-200 hover:text-black transition-all duration-300 ease-in-out rounded-full">
+                  <button className="w-full lg:w-auto px-12 lg:px-16 py-3 lg:py-4 border border-black bg-black text-white hover:bg-gray-200 hover:text-black transition-all duration-300 ease-in-out rounded-full text-sm lg:text-base font-medium text-nowrap">
                     Buy it now
                   </button>
                 </div>
